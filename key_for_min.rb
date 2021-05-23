1a01efd34673b71a6234a80df459adaae1d9df7d
Vincent Tran <vincentuong@yahoo.com>
2021-05-23 22:38:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
     minimum_key = nil
     minimum_value = nil
     hash.each do |key, value|
-        if minimum_value = nil || value < minimum_value
+        if minimum_value == nil || value < minimum_value
         minimum_value = value
         minimum_key = key
         end
